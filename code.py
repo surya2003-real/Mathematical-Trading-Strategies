3a86a5dcefc26eb347aaf1735ea333b6c993184d
surya2003-real <118452600+surya2003-real@users.noreply.github.com>
2023-05-17 15:52:34 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,8 @@
+import pandas as pd
+import yfinance as yf
+
+indexes=['^RUT', '^GSPC', 'AGG', '^BSESN', '^DJI']
+data=yf.download(" ".join(indexes), start='2010-01-01', end='2023-05-01')
+dt=pd.DataFrame(data)
+print(dt.shape)
 
